33a84764a515317f3bb991ba74148ad6d684d5e7
Witty36 <3044760489@qq.com>
2026-04-11 14:52:57 +0800

    docs:vitepress编译增加流程图组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,9 @@ import DefaultTheme from "vitepress/theme";
 import {h} from "vue";
 import BackToTop from "./components/BackToTop.vue";
 import "./custom.css";
+import Mermaid from "./components/Mermaid.vue";
+import Flowchart from './components/Flowchart.vue'
+
 export default {
     extends: DefaultTheme,
 
@@ -9,5 +12,10 @@ export default {
         return h(DefaultTheme.Layout, null, {
             "doc-bottom": () => h(BackToTop),
         });
+    },
+
+    enhanceApp({ app }: { app: any }) {  // 添加类型声明
+        app.component('Mermaid', Mermaid);
+        app.component('Flowchart', Flowchart);
     },
 };
